a5e3f80cc4b2a92a0a3d6f73189eecb8b1340d73
Felix Green <work@felix.green>
2025-09-06 13:45:13 -0400

    about: add page and styles

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { css } from 'ui/css'
+	export let data: { about: { photo: string; blurb: string } }
+	const { about } = data
+</script>
+
+<section
+	class={css({
+		display: 'flex',
+		flexDir: 'column',
+		alignItems: 'center',
+		gap: '6',
+		px: '4',
+		py: '6',
+	})}
+>
+	{#if about.photo}
+		<img
+			src={about.photo}
+			alt="About photo"
+			class={css({ display: 'block', w: 'full', maxW: '320px', h: 'auto' })}
+		/>
+	{/if}
+	{#if about.blurb}
+		<p class={css({ maxW: '65ch', mx: 'auto' })}>{about.blurb}</p>
+	{/if}
+</section>
